ec74bdef524c6b730df21e7c4c7ebe00c9fa7667
muaazshoaib <muaazshoaib1670@gmail.com>
2023-03-26 17:58:09 +0500

    Take Picture with Camera Using Flutter - 10:20

---
+++ README.md
@@ -0,0 +1,3 @@
+# camera_app
+
+[Take Picture with Camera Using Flutter](https://youtu.be/F1djGmAKvRo)
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+late List<CameraDescription> cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CameraApp(),
+    );
+  }
+}
+
+class CameraApp extends StatefulWidget {
+  const CameraApp({super.key});
+
+  @override
+  State<CameraApp> createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  late CameraController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(cameras[0], ResolutionPreset.max);
+    _controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            if (kDebugMode) {
+              print('access was denied');
+            }
+            break;
+          default:
+            if (kDebugMode) {
+              print(e.description);
+            }
+            break;
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: double.infinity,
+            child: CameraPreview(_controller),
+          ),
+        ],
+      ),
+    );
+  }
+}
